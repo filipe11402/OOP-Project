220844044d927ce40742cde043fe1a70796bcf2c
filipe11402 <filipe11402@outlook.pt>
2021-12-05 09:57:56 +0000

    refactored compare method

--- src/Comparators/StudentScoreComparator.java
+++ src/Comparators/StudentScoreComparator.java
@@ -7,6 +7,10 @@ import java.util.Comparator;
 public class StudentScoreComparator implements Comparator<StudentModel> {
     @Override
     public int compare(StudentModel student, StudentModel studentToCompare){
-        return Double.compare(student.getTotalScore(), studentToCompare.getTotalScore());
+        if (student.getTotalScore() == studentToCompare.getTotalScore()) return 0;
+
+        if (student.getTotalScore() < studentToCompare.getTotalScore()) return 1;
+
+        else return -1;
     }
 }
